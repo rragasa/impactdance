b50161373b4e0a3be003753ae0b19fbf81061926
Rachelle Ragasa <rachelle.ragasa@whitbread.com>
2019-04-27 21:31:36 +0100

    adding google maps

--- app/js/main.js
+++ app/js/main.js
@@ -5,3 +5,16 @@ $('.hamburger-menu').click(function(e){
   $('.site-navigation').toggleClass('open-nav');
   $(this).toggleClass('open');
 });
+
+// Google maps
+
+// Initialize and add the map
+function initMap() {
+  // The location of impactdance
+  var impactdance = {lat: 51.549979, lng: -0.234949};
+  // The map, centered at impactdance
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: impactdance});
+  // The marker, positioned at impactdance
+  var marker = new google.maps.Marker({position: impactdance, map: map});
+}
